7a0ebfec34c64f518863a3f34d390d542ca0ef2a
James Aimonetti <james.aimonetti@gmail.com>
2017-01-20 07:53:18 +0000

    update parsing of system values

--- core/kazoo_ast/src/kapps_config_usage.erl
+++ core/kazoo_ast/src/kapps_config_usage.erl
@@ -11,6 +11,8 @@
 -define(FIELD_PROPERTIES, <<"properties">>).
 -define(SYSTEM_CONFIG_DESCRIPTIONS, kz_ast_util:api_path(<<"descriptions.system_config.json">>)).
 
+-define(IGNORE_SYSTEM_VALUE, <<"_system">>).
+
 -spec to_schema_docs() -> 'ok'.
 -spec to_schema_docs(kz_json:object()) -> 'ok'.
 to_schema_docs() ->
@@ -29,7 +31,7 @@ filter_system(JObj) ->
     filter_system_fold(kz_json:get_values(JObj), kz_json:new()).
 
 filter_system_fold({[], []}, JObj) -> JObj;
-filter_system_fold({['_system' | Vc], [ _| Kc]}, JObj) ->
+filter_system_fold({[?IGNORE_SYSTEM_VALUE | Vc], [ _SysKey| Kc]}, JObj) ->
     filter_system_fold({Vc, Kc}, JObj);
 filter_system_fold({[V | Vc], [K | Kc]}, JObj) ->
     case kz_json:is_json_object(V) of
@@ -37,6 +39,7 @@ filter_system_fold({[V | Vc], [K | Kc]}, JObj) ->
         'false' -> filter_system_fold({Vc, Kc}, kz_json:set_value(K, V, JObj))
     end.
 
+-spec static_fields(ne_binary(), kz_json:object()) -> kz_json:object().
 static_fields(Name, JObj) ->
     Id = <<"system_config.", Name/binary>>,
     Description = <<"Schema for ", Name/binary, " system_config">>,
@@ -413,11 +416,11 @@ default_value(?MOD_FUN_ARGS('kz_json', 'from_list', L)) ->
 default_value(?MOD_FUN_ARGS('kz_json', 'new', [])) ->
     kz_json:new();
 default_value(?MOD_FUN_ARGS('kz_util', 'rand_hex_binary', [_Arg])) ->
-    '_system';
+    ?IGNORE_SYSTEM_VALUE;
 default_value(?MOD_FUN_ARGS('kz_util', 'anonymous_caller_id_number', [])) ->
-    default_value(kz_util:anonymous_caller_id_number());
+    kz_util:anonymous_caller_id_number();
 default_value(?MOD_FUN_ARGS('kz_util', 'anonymous_caller_id_name', [])) ->
-    default_value(kz_util:anonymous_caller_id_name());
+    kz_util:anonymous_caller_id_name();
 default_value(?MOD_FUN_ARGS('kz_term', 'to_binary', [Arg])) ->
     default_value(Arg);
 default_value(?MOD_FUN_ARGS('kz_term', 'to_integer', [Arg])) ->
@@ -434,9 +437,9 @@ default_value(?MOD_FUN_ARGS('kapps_config', 'get_integer', [_Category, _Key, Def
 default_value(?MOD_FUN_ARGS('kapps_config', 'get_binary', [_Category, _Key, Default])) ->
     default_value(Default);
 default_value(?MOD_FUN_ARGS(_M, _F, _Args)) ->
-    '_system';
+    ?IGNORE_SYSTEM_VALUE;
 default_value(?FUN_ARGS(_F, _Args)) ->
-    '_system'.
+    ?IGNORE_SYSTEM_VALUE.
 
 default_values_from_list(KVs) ->
     lists:foldl(fun default_value_from_kv/2, kz_json:new(), KVs).
